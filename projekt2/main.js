74a54d6874f489ee6f40da1f40e8be9cda757115
Michal Majer <misa@majer.cz>
2022-04-12 17:43:49 +0200

    p2 timeline on projects

--- projekt2/main.js
+++ projekt2/main.js
@@ -9,8 +9,6 @@ let button_map = {
 let scroll_items = ["#about", "#education", "#projects", "#skills", "#contacts"];
 let scroll_elements = scroll_items.map((selector) => document.querySelector(selector));
 
-console.log(scroll_elements);
-
 let options = {
     root: scroll_area,
     rootMargin: '0px',
@@ -20,7 +18,6 @@ let options = {
 
 function scroll_callback(entries, observer) {
     entries.forEach((entry) => {
-        console.dir(entry);
         let menu_element_id = button_map[entry.target.id];
         let menu_el = document.querySelector(menu_element_id);
         if (entry.isIntersecting) {
@@ -33,3 +30,25 @@ function scroll_callback(entries, observer) {
 
 let observer = new IntersectionObserver(scroll_callback, options);
 scroll_elements.forEach((el) => observer.observe(el));
+
+// Projects scroll progres
+
+window.onscroll = function () { projects_scroll_callback() };
+
+const PROJECTS_RELEVANT_AREA_ID = "projects-sticky-container";
+const PROJECTS_BAR_ID = "timeline-progress";
+
+const TIMELINE_HEIGHT_PX = 170;
+const CIRCLE_R_PX = 9;
+
+function projects_scroll_callback() {
+    let projects_el = document.getElementById(PROJECTS_RELEVANT_AREA_ID);
+    let dom_rect = projects_el.getBoundingClientRect();
+
+    let scrolled = 0;
+    if (dom_rect.y < 0 && dom_rect.y > -dom_rect.height) {
+        scrolled = (-dom_rect.y / (dom_rect.height - TIMELINE_HEIGHT_PX)) * 100;
+    }
+    document.getElementById(PROJECTS_BAR_ID).style.width = scrolled + "%";
+
+}
